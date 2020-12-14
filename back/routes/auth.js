9c83e9cfe4092ea563e4dd0ec467a372e206e7e1
jeyofdev <jgregoire.pro@gmail.com>
2020-12-14 18:12:40 +0100

    quest040 add a new user

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,8 +1,21 @@
+const connection = require('../helpers/config');
 const express = require('express');
 const router = express.Router();
 
-router.post('/signup', function (req, res, next) {
-    res.send('I am in POST signup');
+router.post('/signup', (req, res, next) => {
+    const { email, password, name, lastname } = req.body;
+
+    connection.query(
+        'INSERT INTO users (email, password, name, lastname) VALUES (?, ?, ?, ?)',
+        [email, password, name, lastname],
+        (err, results) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.send('I am in POST signup');
+            }
+        }
+    );
 });
 
 module.exports = router;
