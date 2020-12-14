f1625879fb6c3a98855dde63a44447aaab0e32eb
jeyofdev <jgregoire.pro@gmail.com>
2020-12-14 12:49:14 +0100

    quest030 - init back

---
+++ back/app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const routes = require('./routes');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+    res.send('youhou');
+});
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
